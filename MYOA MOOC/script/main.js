9c405accc42dd74a798d36421d3e9bf24180eca2
José Victor <victdiasmd@gmail.com>
2022-05-28 08:48:19 -0300

    Chatter App, Week 4, Challenge 4

--- MYOA MOOC/script/main.js
+++ MYOA MOOC/script/main.js
@@ -1,110 +1,126 @@
 // object that has all useful info about the app channels
-const channels = {
-    id: "",
-    name: "",
-    favorite: false, // True when is favorite and false when its not
-};
-
-// create every channel object
-const ch1 = Object.create(channels);
-const ch2 = Object.create(channels);
-const ch3 = Object.create(channels);
-const ch4 = Object.create(channels);
+const channels = {};
 
 /* Variables */
-let selectedChannel = "ch1";
+let selectedChannel = 1;
 
 /* Functions */
 // executes when the app is loaded
 function app_alive() {
     console.log("App is alive!");
-    get_channels_info();
+    createChannelsObjects();
 }
 
-// get all channels info
-function get_channels_info() {
-    // get all channels exist
+// create an object to hold all channels info
+function createChannelsObjects() {
     elements = document.querySelectorAll(".channel-list li span");
-    // for every chanel
-    for (var i = 0; i < elements.length; i++) {
-        switch (i) {
-            case 0:
-                ch1.id = elements[i].id;
-                ch1.name = elements[i].textContent;
-                ch1.favorite = false;
-                break;
-            case 1:
-                ch2.id = elements[i].id;
-                ch2.name = elements[i].textContent;
-                ch2.favorite = true;
-                break;
-            case 2:
-                ch3.id = elements[i].id;
-                ch3.name = elements[i].textContent;
-                ch3.favorite = false;
-                break;
-            case 3:
-                ch4.id = elements[i].id;
-                ch4.name = elements[i].textContent;
-                ch4.favorite = false;
-                break;
-
-            default:
-                break;
-        }
+    for (let index = 0; index < elements.length; index++) {
+        channels[index] = {
+            id: elements[index].id,
+            name: elements[index].textContent,
+            favorite: false,
+        };
     }
+    console.log(channels);
 }
 
-function get_selected() {
-    if (selectedChannel == "ch1") return ch1;
-    else if (selectedChannel == "ch2") return ch2;
-    else if (selectedChannel == "ch3") return ch3;
-    else if (selectedChannel == "ch4") return ch4;
-}
-
+// changes channel and highlight it
 function switchChannel(channel) {
-    console.log("Selected channel: " + channel.name);
-
     // remove o último selecionado
     document.getElementsByClassName("selected")[0].classList.remove("selected");
+    // document.getElementsByClassName("mes-outgoing").classList.add(".hide-intes");
 
-    selectedChannel = channel.id;
+    let channels_lenght = Object.keys(channels).length;
+    // acha as informações da id passada
+    for (let index = 0; index < channels_lenght; index++) {
+        if (channel.id == channels[index].id) {
+            // salva o index do canal selecionado
+            selectedChannel = index;
+        }
+    }
 
     // hightlight no canal clicado
     document
-        .getElementById(selectedChannel)
+        .getElementById(channels[selectedChannel].id)
         .parentElement.classList.add("selected");
 
     // pega o titulo na tela das mensagens e substitui pelo cannal selecionado
     showHeader(channel);
 
     // Verifica se o canal está favorito
-    isFavorite(channel);
-    // console.log(get_selected());
+    isFavorite();
 }
 
+// change the header to the selected channel name
 function showHeader(channel) {
-    document.getElementById("channelName").innerHTML = channel.name;
+    document.getElementById("channelName").innerHTML = channel.textContent;
 }
 
-function isFavorite(channel) {
-    if (channel.favorite) {
+// verify is channel is favorite
+function isFavorite() {
+    if (channels[selectedChannel].favorite) {
         document.getElementById("favorite-bt").innerHTML = "favorite";
     } else {
         document.getElementById("favorite-bt").innerHTML = "favorite_border";
     }
 }
 
-function add_to_fav(channel) {
-    if (channel.favorite == false) {
-        console.log("Adding " + channel.name + "to favorites");
-        channel.favorite = true;
+// add or remove channels from favorites
+function add_to_fav() {
+    if (channels[selectedChannel].favorite == false) {
+        console.log(
+            "Adding " + channels[selectedChannel].name + " to favorites"
+        );
+        channels[selectedChannel].favorite = true;
         document.getElementById("favorite-bt").innerHTML = "favorite";
     } else {
-        console.log("Removing " + channel.name + "to favorites");
-        channel.favorite = false;
+        console.log(
+            "Removing " + channels[selectedChannel].name + " from favorites"
+        );
+        channels[selectedChannel].favorite = false;
         document.getElementById("favorite-bt").innerHTML = "favorite_border";
     }
-    // console.log(document.getElementById(channel));
-    isFavorite(channel);
+    isFavorite();
 }
+
+function sendMessage() {
+    var current = new Date();
+    // pega a mensagem escrita
+    var messageText = document.getElementById("message-input").value;
+    console.log("Message: " + messageText);
+    let messageString;
+    messageString =
+        `<div class="incomming">
+            <div>
+                <div class="mes-content-self">
+                    <p>` +
+        messageText +
+        `</p>
+                </div>
+
+                <small>` +
+        current.getHours() +
+        ":" +
+        current.getMinutes() +
+        `</small>
+            </div>
+            <div class="inc-icon">
+                <i class="material-icons">account_circle</i>
+            </div>
+        </div>
+    `;
+    const div = document.querySelector(".all-mes-wraper");
+    if (messageText != "") {
+        div.innerHTML += messageString;
+    }
+
+    document.getElementById("message-input").value = "";
+    bottom();
+}
+
+// auto scroll page
+function bottom() {
+    console.log("Bottom");
+    document.getElementById("bottom").scrollIntoView();
+}
+
